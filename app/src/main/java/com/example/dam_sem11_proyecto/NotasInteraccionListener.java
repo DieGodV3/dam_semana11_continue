586ec9d3f6d4f672156d3640e01a9747ea439faf
diegod <diego.qc2013@gmail.com>
2023-06-26 17:27:16 -0500

    falta

---
+++ app/src/main/java/com/example/dam_sem11_proyecto/NotasInteraccionListener.java
@@ -0,0 +1,9 @@
+package com.example.dam_sem11_proyecto;
+
+public interface NotasInteraccionListener {
+    void editNotaClick(Nota nota);
+    void eliminaNotaClick (Nota nota);
+    void favoritaNotaClick (Nota nota);
+}
+
+
